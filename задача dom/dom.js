88e7627c702e5d496dc69ea7960de92e590557e1
SergeyBerez <sergeyberezovsky1981@gmail.com>
2019-08-16 09:58:58 +0300

    1

--- задача dom/dom.js
+++ задача dom/dom.js
@@ -1,49 +1,37 @@
+// Создать модуль, который экспортирует функцию `prepend`
+// prepend имеет два параметра, в которые нужно передать элементы
+// Задача функции - вставить второй элемент в начало первого. Например:
+// `prepend(container, newElement)` - newElement должен быть добавлен в начало элемента container.
+
 let cont = document.querySelector('.conteiner');
-
-// let div = document.querySelectorAll('.box');
-// for (let i = 0; i < div.length; i++) {
-//   div[i].onclick = addBackground;
+// function addBackground(e) {
+//   e.target.classList.toggle('background');
+//   console.log(e.currentTarget);
 // }
+// cont.addEventListener('click', addBackground);
 
-function addBackground(e) {
-  e.target.classList.toggle('background');
-  console.log(e.currentTarget);
-}
-
-cont.addEventListener('click', addBackground);
-
-let countelement = 0;
-let countenode = 0;
-for (let i = 0; i < cont.childNodes.length; i++) {
-  var node = cont.childNodes[i].nodeType;
-  if (node == 1) {
-    countelement++;
-  }
-  if (node == 3) {
-    countenode++;
-  }
-}
-console.log(cont.childNodes);
+// let countelement = 0;
+// let countenode = 0;
+// for (let i = 0; i < cont.childNodes.length; i++) {
+//   var node = cont.childNodes[i].nodeType;
+//   if (node == 1) {
+//     countelement++;
+//   }
+//   if (node == 3) {
+//     countenode++;
+//   }
+// }
+// console.log(cont.childNodes);
 //console.log('element == ' + countelement);
 //console.log('text node == ' + countenode);
+//    узел дом .childNodes[0] элемент перед которым вставляетс новый елемент
+function prepend(cont, elem) {
+  cont.insertBefore(elem, cont.childNodes[0]); // cont.insertBefore cont родительский елемент
 
-function addElement(dom, elem) {
-  dom.parentNode.insertBefore(elem, dom);
-
-  //console.log(dom.parentNode);
+  console.log(cont.childNodes);
 }
+let div = document.querySelector('.box4');
+
 let newElement = document.createElement('span');
 newElement.textContent = 'привет';
-addElement(cont, newElement);
-
-function deleteTextNodes(domElement) {
-  for (let i = 0; i < domElement.childNodes.length; i++) {
-    var node = domElement.childNodes[i].nodeType;
-    if (node == 3) {
-      domElement.removeChild(domElement.childNodes[i]);
-    }
-  }
-}
-deleteTextNodes(cont);
-console.log(document.body.children[0].childNodes);
-console.log(cont.childNodes);
+prepend(cont, div); // добавляем элемент
